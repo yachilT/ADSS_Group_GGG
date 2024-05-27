90e1d8d57c449a17afa70fc60e980f503f2eecc6
roioha3 <roioha@post.bgu.ac.il>
2024-05-27 12:26:56 +0300

    Created service_layer and presentation_layer. moved shipmentSchduler to service_layer

--- dev/src/domain_layer/Shipment.java
+++ dev/src/domain_layer/Shipment.java
@@ -2,6 +2,8 @@ package domain_layer;
 
 import java.sql.Time;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.Map;
 
 public class Shipment {
@@ -10,12 +12,14 @@ public class Shipment {
     private Time departureTime;
     private Map<Site, Float> weights;
     private Map<Site, DestinationDocument> destinations;
-    public Shipment(int shipmentId, Date shipmentDate, Time departureTime, Map<Site, Float> weights, Map<Site, DestinationDocument> destinations){
+    public Shipment(int shipmentId, Date shipmentDate, Time departureTime, Map<Site, DestinationDocument> destinations){
         this.shipmentId = shipmentId;
         this.shipmentDate = shipmentDate;
         this.departureTime = departureTime;
-        this.weights = weights;
-        this.destinations = destinations;
+        this.weights = new HashMap<>();
+        this.destinations = new HashMap<>();
     }
+    public void add(Site site){
 
+    }
 }
