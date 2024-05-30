b760e53b76a1dab53ce66d7f5e6498db240cb5fb
Your Name <yachil@post.bgu.ac.il>
2024-05-30 12:04:41 +0300

    declared skeleton of ShipmentTrackerService

--- dev/src/service_layer/ShipmentTrackerService.java
+++ dev/src/service_layer/ShipmentTrackerService.java
@@ -1,11 +1,47 @@
 package service_layer;
 
+import domain_layer.ShipmentScheduler;
 import domain_layer.ShipmentTracker;
+import domain_layer.TruckFacade;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.ResourceBundle;
 
 public class ShipmentTrackerService {
-    private ShipmentTracker shipmentTracker;
-    public ShipmentTrackerService(ShipmentTracker shipmentTracker){
-        this.shipmentTracker = shipmentTracker;
+    private List<ShipmentTracker> shipmentTrackers;
+    private ShipmentScheduler shipmentScheduler;
+    private TruckFacade truckFacade;
+    public ShipmentTrackerService(ShipmentScheduler scheduler, TruckFacade truckFacade){
+        this.shipmentScheduler = scheduler;
+        this.shipmentTrackers = new ArrayList<>();
+        this.truckFacade = truckFacade;
+    }
+
+    public Response trackShipment(int shipmentId){
+        try {
+            shipmentTrackers.add(new ShipmentTracker(shipmentScheduler.getShipment(shipmentId)));
+        }
+        catch (NoSuchElementException e){
+            //return response error
+        }
+    }
+
+    public Response changeTruck() {
+
+    }
+
+    public Response removeDestination(SiteToSend site) {
+
+    }
+
+    public Response changeDestination(SiteToSend oldSite, SiteToSend newSite) {
+
+    }
+
+    public Response productsToRemain(List<ProductToSend> remainingProducts) {
+
     }
 
 
