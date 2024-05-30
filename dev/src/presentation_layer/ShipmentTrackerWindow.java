253577a6d68c1dca6c69f77da74d8a887be122dd
roioha3 <roioha@post.bgu.ac.il>
2024-05-30 11:48:43 +0300

    finished ScedueleShipmentWindow

---
+++ dev/src/presentation_layer/ShipmentTrackerWindow.java
@@ -0,0 +1,23 @@
+package presentation_layer;
+
+public class ShipmentTrackerWindow extends Window {
+    private int shipmentId;
+    public ShipmentTrackerWindow(Integer shipmentId) {
+        this.shipmentId = shipmentId;
+    }
+
+    @Override
+    public Window run(Controller controller) {
+        return null;
+    }
+
+    @Override
+    public void close() {
+
+    }
+
+    @Override
+    public void open() {
+
+    }
+}
