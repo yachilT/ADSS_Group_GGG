8f7182198021e03cb45d57310bee2cf2b9fcc2d2
roioha3 <roioha@post.bgu.ac.il>
2024-05-30 12:04:49 +0300

    editing Controller.java

--- dev/src/presentation_layer/Controller.java
+++ dev/src/presentation_layer/Controller.java
@@ -1,9 +1,6 @@
 package presentation_layer;
 
-import domain_layer.AreaFacade;
-import domain_layer.DriverFacade;
-import domain_layer.ShipmentScheduler;
-import domain_layer.TruckFacade;
+import domain_layer.*;
 import service_layer.*;
 
 import java.util.Scanner;
@@ -12,11 +9,15 @@ public class Controller {
     public Scanner scanner;
     public AreaService areaService;
     public ShipmentSchedulerService shipmentSchedulerService;
+    public ShipmentTrackerService shipmentTrackerService;
     public Controller(){
         scanner = new Scanner(System.in);
         AreaFacade areaFacade = new AreaFacade();
         areaService = new AreaService(areaFacade);
-        shipmentSchedulerService = new ShipmentSchedulerService(new ShipmentScheduler(new DriverFacade(), new TruckFacade()), areaFacade);
+        TruckFacade truckFacade = new TruckFacade();
+        ShipmentScheduler shipmentScheduler = new ShipmentScheduler(new DriverFacade(),truckFacade);
+        shipmentSchedulerService = new ShipmentSchedulerService(shipmentScheduler, areaFacade);
+        shipmentTrackerService = new ShipmentTrackerService(shipmentScheduler, truckFacade);
     }
     public void run(){
         Window window = new MainMenuWindow();
