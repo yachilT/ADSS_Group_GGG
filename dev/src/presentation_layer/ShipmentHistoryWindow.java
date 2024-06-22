716c41cff54ee6848443a134bf0e0e15194ea49f
roioha3 <roioha@post.bgu.ac.il>
2024-06-22 21:58:39 +0300

    fixed some problems

--- dev/src/presentation_layer/ShipmentHistoryWindow.java
+++ dev/src/presentation_layer/ShipmentHistoryWindow.java
@@ -4,6 +4,7 @@ import domain_layer.DestinationDocument;
 import domain_layer.ShipmentDocument;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 import java.util.stream.IntStream;
 
@@ -17,30 +18,43 @@ public class ShipmentHistoryWindow implements   Window{
         do {
             try { Thread.sleep(1000); } catch (InterruptedException e) {}
             System.out.println("You have the following options:\n" + "\n" + "1. display destination documents of current shipment\n" + "2. Choose another document\n" + "0. Return to the main menu");
-            int choice = controller.scanner.nextInt();
-            switch (choice) {
-                case 1 -> {
-                    DestinationDocument destDoc = selectDestinationDocument(controller.shipmentHistoryService.getDestinationDocuments(document), controller.scanner);
-                    System.out.println(destDoc);
+            try {
+                int choice = controller.scanner.nextInt();
+                switch (choice) {
+                    case 1 -> {
+                        DestinationDocument destDoc = selectDestinationDocument(controller.shipmentHistoryService.getDestinationDocuments(document), controller.scanner);
+                        System.out.println(destDoc);
+                    }
+                    case 2 -> {
+                        document = selectDocument(controller.shipmentHistoryService.getDocuments(), controller.scanner);
+                        System.out.println(document);
+                    }
+                    case 0 -> {
+                        return new MainMenuWindow();
+                    }
+                    default -> System.out.println("Error: Invalid Selection, try again.\n");
                 }
-                case 2 -> {
-                    document = selectDocument(controller.shipmentHistoryService.getDocuments(), controller.scanner);
-                    System.out.println(document);
-                }
-                case 0 -> {
-                    return new MainMenuWindow();
-                }
-                default -> System.out.println("Error: Invalid Selection, try again.\n");
+            }
+            catch (NoSuchElementException e){
+                System.out.println("Invalid input: Please enter an integer.")
             }
 
         } while (true);
     }
 
     private DestinationDocument selectDestinationDocument(List<DestinationDocument> documents, Scanner scanner) {
-        int documentIndex;
+
+        int documentIndex = -1;
         do {
             IntStream.range(0, documents.size()).forEach(index -> System.out.println((index + 1) + ". ID: " + documents.get(index).getId() + " address: " + documents.get(index).getAddress()));
-            documentIndex = scanner.nextInt();
+            try {
+                documentIndex = scanner.nextInt();
+            }
+            catch (NoSuchElementException e){
+                System.out.println("Invalid input: Please enter an integer.");
+                continue;
+            }
+
             if (documentIndex <= 0 || documentIndex > documents.size())
                 System.out.println("Error: Invalid Selection\n");
         } while (documentIndex <= 0 || documentIndex > documents.size());
@@ -50,12 +64,17 @@ public class ShipmentHistoryWindow implements   Window{
     private ShipmentDocument selectDocument(List<ShipmentDocument> documents, Scanner scanner) {
         if (documents.size() > 0) {
             System.out.println("Please choose a document by entering its number");
-            int documentIndex;
+            int documentIndex = -1;
             do {
                 IntStream.range(0, documents.size()).forEach(index -> System.out.println((index + 1) + ". ID: " + documents.get(index).getId() + " Origin address: " + documents.get(index).getOriginAddress()));
-                documentIndex = scanner.nextInt();
-                if (documentIndex <= 0 || documentIndex > documents.size())
-                    System.out.println("Error: Invalid Selection\n");
+                try {
+                    documentIndex = scanner.nextInt();
+                    if (documentIndex <= 0 || documentIndex > documents.size())
+                        System.out.println("Error: Invalid Selection\n");
+                }
+                catch (NoSuchElementException e){
+                    System.out.println("Invalid input: Please enter an integer.");
+                }
             } while (documentIndex <= 0 || documentIndex > documents.size());
 
             return documents.get(documentIndex - 1);
